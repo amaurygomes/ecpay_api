29ad07cdfef2a39c3287661ef862b4dec76c1417
Amaury Gomes Ibanez <amaury_mju@hotmail.com>
2025-06-26 18:28:38 -0400

    feat: inicializa projeto com dependências principais

---
+++ src/main/java/br/com/amaurygomes/ecpay/EcpayApplication.java
@@ -0,0 +1,13 @@
+package br.com.amaurygomes.ecpay;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcpayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcpayApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/amaurygomes/ecpay/EcpayApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.amaurygomes.ecpay;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcpayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
