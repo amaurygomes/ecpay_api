93ea67a0d7d5e6d9c4b44e165979aa87812f8d01
Amaury Gomes Ibanez <amaury_mju@hotmail.com>
2025-06-27 07:23:47 -0400

    fix(entity): ajusta anotações Lombok para construção e acesso

--- src/main/java/br/com/amaurygomes/ecpay/user/entity/DeliveryUser.java
+++ src/main/java/br/com/amaurygomes/ecpay/user/entity/DeliveryUser.java
@@ -3,11 +3,19 @@ package br.com.amaurygomes.ecpay.user.entity;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.experimental.SuperBuilder;
 
 @Entity
 @Table(name = "delivery_users")
+@Setter
+@Getter
+@SuperBuilder
+@NoArgsConstructor
 public class DeliveryUser extends User{
     @Column(nullable = false)
     private Long vtr;
-    private PlatformStatus platformStatus;
+    private PlatformStatus platformStatus = PlatformStatus.ACTIVE;
 }
--- src/main/java/br/com/amaurygomes/ecpay/user/entity/User.java
+++ src/main/java/br/com/amaurygomes/ecpay/user/entity/User.java
@@ -1,12 +1,18 @@
 package br.com.amaurygomes.ecpay.user.entity;
 
 import jakarta.persistence.*;
+import lombok.*;
+import lombok.experimental.SuperBuilder;
 
 import java.util.UUID;
 
 @Entity
 @Table(name = "users")
 @Inheritance(strategy = InheritanceType.JOINED)
+@NoArgsConstructor
+@SuperBuilder
+@Getter
+@Setter
 public abstract class User {
     @Id
     @GeneratedValue(strategy = GenerationType.UUID)
@@ -17,9 +23,6 @@ public abstract class User {
     private String login;
     @Column(nullable = false)
     private String encodedPassword;
-    @Enumerated(EnumType.STRING)
-    @Column(nullable = false)
-    private Role role;
-    private boolean isActive;
+    private boolean isActive = true;
 
 }
