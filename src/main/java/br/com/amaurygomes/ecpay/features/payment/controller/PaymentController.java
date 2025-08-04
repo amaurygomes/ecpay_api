af7ce31a16b8f7868e69d3f40ba026410c2b4f4d
Amaury Gomes Ibanez <amaury_mju@hotmail.com>
2025-08-04 19:16:31 -0400

    feat(mercadopago): Adiciona controller para gerar pagamento via pix do mercado pago

--- src/main/java/br/com/amaurygomes/ecpay/features/payment/controller/PaymentController.java
+++ src/main/java/br/com/amaurygomes/ecpay/features/payment/controller/PaymentController.java
@@ -1,18 +1,27 @@
 package br.com.amaurygomes.ecpay.features.payment.controller;
 
 import br.com.amaurygomes.ecpay.features.payment.service.PaymentService;
+import br.com.amaurygomes.ecpay.gateway.mercadopago.MercadoPagoGeneratePixPayment;
+import br.com.amaurygomes.ecpay.gateway.mercadopago.MercadoPagoPixPaymentRequest;
+import br.com.amaurygomes.ecpay.gateway.mercadopago.MercadoPagoPixPaymentResponse;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/payment")
+@RequestMapping("/api/payment/generate_pix")
 @RequiredArgsConstructor
 public class PaymentController {
     private final PaymentService paymentService;
+    private final MercadoPagoGeneratePixPayment mercadoPagoGeneratePixPayment;
 
-    public ResponseEntity refundPayment() {
-        return ResponseEntity.ok().build();
+    @PostMapping("/mercadopago")
+    public ResponseEntity<MercadoPagoPixPaymentResponse> generatePixPayment(@RequestBody @Valid MercadoPagoPixPaymentRequest request){
+        return ResponseEntity.ok(mercadoPagoGeneratePixPayment.generatePix(request.userId(), request.billingCycle()));
     }
+
 }
