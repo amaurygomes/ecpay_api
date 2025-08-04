7b450f4660d4d7deb77a07b961d10cf412e7d165
Amaury Gomes Ibanez <amaury_mju@hotmail.com>
2025-08-04 19:23:31 -0400

    refactor(controller): Altera prefixo dos endpoints da Aplicação

--- src/main/java/br/com/amaurygomes/ecpay/features/system/controller/SettingsController.java
+++ src/main/java/br/com/amaurygomes/ecpay/features/system/controller/SettingsController.java
@@ -11,7 +11,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/system/settings")
+@RequestMapping("/api/system/settings")
 @RequiredArgsConstructor
 public class SettingsController {
     private final SettingsService settingsService;
